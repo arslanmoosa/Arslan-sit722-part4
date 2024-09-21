5470f08dc055ab6af0dd7780ee33177186272316
Arslan Ahmad <arslanmoosa328@gmail.com>
2024-09-21 21:00:00 +0500

    last commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,19 +21,13 @@ kubectl get pods
 kubectl get services
 kubectl get deployments
 sleep 20
+echo "IPs"
 
-# Get External IP addresses
 BOOK_CATALOG_IP=$(kubectl get service book-catalog -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 INVENTORY_MANAGEMENT_IP=$(kubectl get service inventory-management -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-# Get External Ports
 BOOK_CATALOG_PORT=$(kubectl get service book-catalog -o jsonpath='{.spec.ports[0].port}')
 INVENTORY_MANAGEMENT_PORT=$(kubectl get service inventory-management -o jsonpath='{.spec.ports[0].port}')
-# Define Service URLs
 BOOK_CATALOG_URL="http://$BOOK_CATALOG_IP:$BOOK_CATALOG_PORT/docs"
 INVENTORY_MANAGEMENT_URL="http://$INVENTORY_MANAGEMENT_IP:$INVENTORY_MANAGEMENT_PORT/docs"
-
-echo "---------------------------------- Service URLS -----------------------------------"
-echo "-----------------------------------------------------------------------------------"
 echo BOOK CATALOG URL = "http://$BOOK_CATALOG_IP:$BOOK_CATALOG_PORT/docs"
-echo INVENTORY MANAGEMENT URL = "http://$INVENTORY_MANAGEMENT_IP:$INVENTORY_MANAGEMENT_PORT/docs"
-echo -e "-----------------------------------------------------------------------------------\n"
+echo INVENTORY MANAGEMENT URL = "http://$INVENTORY_MANAGEMENT_IP:$INVENTORY_MANAGEMENT_PORT/inventories"
