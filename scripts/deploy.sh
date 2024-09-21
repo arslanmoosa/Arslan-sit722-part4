b0a93dacdd1b525d260d3ade0c4be11a74c1e958
Arslan Ahmad <arslanmoosa328@gmail.com>
2024-09-21 18:56:07 +0500

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,5 +15,23 @@ echo "Delaying 20 seconds to get the deployments done"
 sleep 20
 
 kubectl get pods
+
 kubectl get services
 kubectl get deployments
+sleep 20
+
+# Get External IP addresses
+BOOK_CATALOG_IP=$(kubectl get service book-catalog-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+INVENTORY_MANAGEMENT_IP=$(kubectl get service inventory-management-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+# Get External Ports
+BOOK_CATALOG_PORT=$(kubectl get service book-catalog-service -o jsonpath='{.spec.ports[0].port}')
+INVENTORY_MANAGEMENT_PORT=$(kubectl get service inventory-management-service -o jsonpath='{.spec.ports[0].port}')
+# Define Service URLs
+BOOK_CATALOG_URL="http://$BOOK_CATALOG_IP:$BOOK_CATALOG_PORT/docs"
+INVENTORY_MANAGEMENT_URL="http://$INVENTORY_MANAGEMENT_IP:$INVENTORY_MANAGEMENT_PORT/docs"
+
+echo "---------------------------------- Service URLS -----------------------------------"
+echo "-----------------------------------------------------------------------------------"
+echo BOOK CATALOG URL = "http://$BOOK_CATALOG_IP:$BOOK_CATALOG_PORT/docs"
+echo INVENTORY MANAGEMENT URL = "http://$INVENTORY_MANAGEMENT_IP:$INVENTORY_MANAGEMENT_PORT/docs"
+echo -e "-----------------------------------------------------------------------------------\n"
